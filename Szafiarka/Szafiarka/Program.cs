392995586863e5ee0cdcf33db9e8cf43b9d3fbe0
Szymon <Szymon@SZYMON-KOMPUTER>
2017-10-12 23:09:33 +0200

    added splashscreen and class to retrive assembly data

--- Szafiarka/Szafiarka/Program.cs
+++ Szafiarka/Szafiarka/Program.cs
@@ -13,7 +13,7 @@ namespace Szafiarka
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Splashscreen.Splashscreen());
         }
     }
 }
---
+++ Szafiarka/Szafiarka/Splashscreen/Splashscreen.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Szafiarka.Splashscreen
+{
+    public partial class Splashscreen : Form
+    {
+        public Splashscreen()
+        {
+            RetrievingAssemblyData retrivingAssemblyData = new RetrievingAssemblyData();
+            InitializeComponent(retrivingAssemblyData);
+        }
+    }
+}
