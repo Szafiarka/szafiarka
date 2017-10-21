561c9d8bf5e2ec981d1278aa6807a6cebe0daa80
Szymon <Szymon@SZYMON-KOMPUTER>
2017-10-22 01:05:33 +0200

    added InitializeMenuButtons()

--- Szafiarka/Szafiarka/Classes/PanelMenu.cs
+++ Szafiarka/Szafiarka/Classes/PanelMenu.cs
@@ -3,10 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace Szafiarka.Classes
 {
-    class PanelMenu
+    class PanelMenu : Panel
     {
+        public PanelMenu()
+        {
+            BackColor = System.Drawing.Color.FromArgb(1, 168, 204);
+            Location = new System.Drawing.Point(0, 0);
+            Size = new System.Drawing.Size(100, 766);
+            Name = "pMenu";
+        }
     }
 }
--- Szafiarka/Szafiarka/Classes/menuButton.cs
+++ Szafiarka/Szafiarka/Classes/menuButton.cs
@@ -9,20 +9,43 @@ namespace Szafiarka.Classes
         private static string PATHTOSTART = "";
         private static string PATHTOSEARCH = "";
         private static string PATHTOADD = "";
-        private static string PATHTOEDIT= "";
+        private static string PATHTOEDIT = "";
         private static string PATHTODELETE = "";
         private static string PATHTOEXIT = "";
-        private static string[,] MENUBUTTONSNAMES = { { "home", "Start", PATHTOSTART }, { "search", "Wyszukaj", PATHTOSEARCH}, 
-            { "add", "Dodaj", PATHTOADD}, {"edit", "Edytuj",  PATHTOEDIT}, { "delete", "Usun", PATHTODELETE}, { "exit", "Wyjście", PATHTOEXIT} };
+        private static string[,] MENUBUTTONSNAMES = {
+            { "home", "Start", PATHTOSTART },
+            { "search", "Wyszukaj", PATHTOSEARCH},
+            { "add", "Dodaj", PATHTOADD},
+            { "edit", "Edytuj",  PATHTOEDIT},
+            { "delete", "Usuń", PATHTODELETE},
+            { "exit", "Wyjście", PATHTOEXIT}
+        };
 
         public MenuButton() : base()
         {
             UseVisualStyleBackColor = false;
-            Size = new Size(80, 80);
-            BackColor = Color.FromArgb(200, 150, 75);
+            Size = new Size(100, 100);
             FlatStyle = FlatStyle.Flat;
             FlatAppearance.BorderSize = 0;
             TextAlign = ContentAlignment.BottomCenter;
+            Font = new Font(this.Font.Name, 10);
+        }
+
+        public void InitializeMenuButtons(Form form, Panel pMenu)
+        {
+            for (int i = 0; i < MENUBUTTONSNAMES.Length / 3; i++)
+            {
+                var button = new MenuButton()
+                {
+                    Location = new Point(0, 100 * i),
+                    BackColor = pMenu.BackColor,
+                    Name = MENUBUTTONSNAMES[i, 0],
+                    Text = MENUBUTTONSNAMES[i, 1]
+                };
+
+                form.Controls.Add(button);
+                pMenu.Controls.Add(button);
+            }
         }
     }
 }
