212c713f83a746ce93008a99faee5e7c431bd31c
Bartek <Bartek@THINKPAD-BARTEK>
2017-11-13 19:22:20 +0100

    Dodanie zakładek do forma opcji.

---
+++ Szafiarka/Szafiarka/Classes/TabControls/OptionsTabControl/OptionsTabPages.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Drawing;
+using System.ComponentModel;
+using System.Reflection;
+
+namespace Szafiarka.Classes
+{
+    partial class OptionsTabPages: TabPage
+    {
+        private enum tabsNames
+        {
+            backup, appearance, contact, about_program
+        }
+
+        private static string[,] TABPAGESNAMES = {
+            { tabsNames.backup.ToString(), "Kopia zapasowa" },
+            { tabsNames.appearance.ToString(), "Wygląd"},
+            { tabsNames.contact.ToString(), "Kontakt"},
+            { tabsNames.about_program.ToString(), "O programie"}
+        };
+
+        static public void InitializeOptionsTabPages(OptionsTabControl tabControl)
+        {
+            for (int i = 0; i < TABPAGESNAMES.Length / 2; i++)
+            {
+                var tab = new OptionsTabPages()
+                {
+                    Name = TABPAGESNAMES[i, 0],
+                    Text = TABPAGESNAMES[i, 1]
+                };
+                tabControl.Controls.Add(tab);
+            }
+        }
+
+        public OptionsTabPages()
+        {
+            //kolory
+        }
+    }
+}
--- Szafiarka/Szafiarka/Forms/OptionsForm/OptionsForm.cs
+++ Szafiarka/Szafiarka/Forms/OptionsForm/OptionsForm.cs
@@ -8,11 +8,11 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace Szafiarka.Forms.ToolsForm
+namespace Szafiarka.Forms.OptionsForm
 {
-    public partial class ToolsForm : Form
+    public partial class OptionsForm : Form
     {
-        public ToolsForm()
+        public OptionsForm()
         {
             InitializeComponent();
         }
