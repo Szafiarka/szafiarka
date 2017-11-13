af4d9917d82aaa51bb25c98002404ee4735e3956
Szymon <Szymon@SZYMON-KOMPUTER>
2017-11-13 22:48:14 +0100

    database adjusments

--- Szafiarka/Szafiarka/Properties/Settings.Designer.cs
+++ Szafiarka/Szafiarka/Properties/Settings.Designer.cs
@@ -9,31 +9,36 @@
 //------------------------------------------------------------------------------
 
 namespace Szafiarka.Properties {
-    
-    
+
+
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "15.3.0.0")]
-    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
-        
+    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase
+    {
+
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
-        
-        public static Settings Default {
-            get {
+
+        public static Settings Default
+        {
+            get
+            {
                 return defaultInstance;
             }
         }
-        
+
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
         [global::System.Configuration.DefaultSettingValueAttribute("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\\database.mdf;" +
             "Integrated Security=True")]
-        public string databaseConnectionString {
-            get {
+        public string databaseConnectionString
+        {
+            get
+            {
                 return ((string)(this["databaseConnectionString"]));
             }
         }
-        
+
 
     }
 }
