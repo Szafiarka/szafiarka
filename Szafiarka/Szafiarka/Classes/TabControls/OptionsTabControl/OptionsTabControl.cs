a5cb8a41858144b80694901638a0d809e60a89e1
Bartek <Bartek@THINKPAD-BARTEK>
2017-11-14 19:54:35 +0100

    Poprawa kart Opcji

--- Szafiarka/Szafiarka/Classes/TabControls/OptionsTabControl/OptionsTabControl.cs
+++ Szafiarka/Szafiarka/Classes/TabControls/OptionsTabControl/OptionsTabControl.cs
@@ -15,6 +15,7 @@ namespace Szafiarka.Classes
             this.Size = new Size(1000, 647);//(900, 582);
             this.Location = new Point(0,0);//(50, 32);     //testowy rozmiar i lokalizacja
             this.Name = "OptionsTabControl";
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
         }
     }
 }
